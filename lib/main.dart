82dbbf864fb8d55ce54e733bcc5412c3d97edb32
Bande Shivraj <NH2322@nslhub.in>
2023-10-26 20:24:10 +0530

    create file to fetch data

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,45 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:dynamic_widget/dynamic_widget.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final String jsonWidgetTree = '''
-{
-  "type": "Column",
-  "crossAxisAlignment": "start",
-  "mainAxisAlignment": "start",
-  "mainAxisSize": "max",
-  "textBaseline": "alphabetic",
-  "textDirection": "ltr",
-  "verticalDirection": "down",
-  "children":[
-    {
-      "type" : "Text",
-      "data" : "Flutter"
-    },
-    {
-      "type" : "Text",
-      "data" : "Demo"
-    }
-  ]
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
-''';
+
+class _MyAppState extends State<MyApp> {
+  String? jsonWidgetTree;
+
+  Future<String> generateCode() async {
+    try {
+      final String jsonstring = await rootBundle.loadString('json/code.json');
+      Map<String, dynamic> jsondata = jsonDecode(jsonstring);
+      print(jsondata["widgetkey"]);
+      return jsonEncode(jsondata["widgetkey"]);
+    } catch (e) {
+      print("Error is : ${e}");
+    }
+    return "sdfgdfsg";
+  }
+
+  getCodeFromFile() async {
+    final val = await generateCode();
+    setState(() {
+      jsonWidgetTree = val;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCodeFromFile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -60,50 +72,6 @@ class MyApp extends StatelessWidget {
       return Container(); // Return a default widget when null is encountered.
     }
   }
-  //  Future<Widget> _buildWidget(BuildContext context,jsonString) async {
-
-  // }
-
-  // Widget buildWidgetsFromJson(String json) {
-  //   try {
-  //     final dynamic parsed = jsonDecode(json);
-  //     if (parsed is Map<String, dynamic> && parsed.containsKey('type')) {
-  //       return _buildWidgetFromJson(parsed);
-  //     }
-  //   } catch (e) {
-  //     print('Failed to parse JSON: $e');
-  //   }
-  //   return Container(); // Default empty container if JSON is invalid
-  // }
-
-  // Widget _buildWidgetFromJson(Map<String, dynamic> json) {
-  //   final String type = json['type'];
-  //   final List<dynamic>? children = json['children'];
-
-  //   if (type == 'Column') {
-  //     return Column(
-  //       children: children?.map((child) => _buildWidgetFromJson(child)).toList() ?? [],
-  //     );
-  //   } else if (type == 'Text') {
-  //     final String data = json['data'];
-  //     final Map<String, dynamic> style = json['style'];
-  //     return Text(
-  //       data,
-  //       style: TextStyle(
-  //         fontSize: style['fontSize']?.toDouble() ?? 14.0,
-  //         // color: Color(int.parse(style['color'].replaceAll("#", "0xFF"), radix: 16)),
-  //       ),
-  //     );
-  //   } else if (type == 'Container') {
-  //     // final Color color = Color(int.parse(json['color'].replaceAll("#", "0xFF"), radix: 16));
-  //     final dynamic child = json['child'];
-  //     return Container(
-  //       // color: color,
-  //       child: _buildWidgetFromJson(child),
-  //     );
-  //   }
-  //   return Container(); // Default empty container if the type is unknown
-  // }
 }
 
 class DefaultClickListener implements ClickListener {
