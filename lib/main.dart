a9063651b0f28ea5044866bf3c91ff2eaa13973a
Bande Shivraj <NH2322@nslhub.in>
2023-10-31 19:38:26 +0530

    changed ui

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String? jsonWidgetTree;
+  String? jsonWidgetTree ;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late DynamicWidgetJsonExportor? _exportor;
 
   Future<String> generateCode() async {
     try {
       CollectionReference users =
           FirebaseFirestore.instance.collection('users');
-      var data = await users.doc("D4Qk8iQtQmN5jAD3exXg").get();
+      var data = await users.doc("YWLMWGt2Zv9qoiCGdRuj").get();
       Map<String, dynamic> clientDetails = data.data() as Map<String, dynamic>;
       // return Text("Full Name: ${data['full_name']} ${data['last_name']}");
       print(clientDetails['clientName']);
+      Map<String, dynamic> projectData = clientDetails["projects"][0];
+      print(projectData["projectId"]);
     } catch (e) {
       print("Error is : ${e}");
     }
-    return "sdfgdfsg";
+    return '''
+{
+  "type": "Container",
+  "color": "#FF00FF",
+  "alignment": "center",
+  "child": {
+    "type": "Text",
+    "data": "Flutter dynamic widget",
+    "maxLines": 3,
+    "overflow": "ellipsis",
+    "style": {
+      "color": "#00FFFF",
+      "fontSize": 20.0
+    }
+  }
+}
+
+''';
   }
 
   getCodeFromFile() async {
@@ -64,7 +84,7 @@ class _MyAppState extends State<MyApp> {
             print(snapshot.error);
           }
           return snapshot.hasData
-              ? SizedBox.expand(
+              ? _exportor = DynamicWidgetJsonExportor(
                   child: snapshot.data,
                 )
               : Text("Loading...");
@@ -88,5 +108,32 @@ class DefaultClickListener implements ClickListener {
   @override
   void onClicked(String? event) {
     print("Receive click event: " + event!);
+  }
+}
+
+class DynamicWidgetJsonExportor extends StatelessWidget {
+  final Widget? child;
+
+  final GlobalKey globalKey = GlobalKey();
+
+  DynamicWidgetJsonExportor({
+    this.child,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: globalKey,
+      child: child,
+    );
+  }
+
+  String exportJsonString() {
+    String rt = "failed to export";
+    globalKey.currentContext!.visitChildElements((element) {
+      rt = jsonEncode(DynamicWidgetBuilder.export(element.widget, null));
+    });
+    return rt;
   }
 }
