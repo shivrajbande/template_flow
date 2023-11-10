60f78c70239fdce03ceb73a3d59ffbb0c8ee0716
hemos811 <811.hemanthkumar@gmail.com>
2023-11-10 05:38:54 +0000

    Added Changed APK files

--- android/app/src/main/kotlin/com/mycompany/demo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/mycompany/zomo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.zomo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
